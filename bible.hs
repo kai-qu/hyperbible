99c350d001235927974748da5668b87c92b6f0a2
katherineye <kye@princeton.edu>
2017-11-25 03:27:55 -0500

    working version

--- bible.hs
+++ bible.hs
@@ -7,7 +7,7 @@ import Debug.Trace
 type Filename = String
 type Verse = String
 type Translation = String
-type Chapter = Int
+type Chapter = String
      
 bible_csv, bible_txt, ecc_txt :: Filename
 bible_csv = "bibles.csv"
@@ -15,7 +15,7 @@ bible_txt = "bibles.txt"
 ecc_txt = "ecc.txt" -- ecclesiastes
 
 gen :: StdGen
-gen = mkStdGen 7 -- 5
+gen = mkStdGen 7
 
 bookName :: String
 bookName = "Ecclesiastes"
@@ -55,22 +55,33 @@ prettyPrint2 s = prettyPrint $ map (\(v,t) -> (v, [t])) s
 
 prettyPrintChapter :: [(Chapter, String)] -> IO ()
 prettyPrintChapter cts = mapM_ printct cts
-                   where printct (c, t) = do putStrLn $ bookName ++ " " ++ show c ++ "\n"
+                   where printct (c, t) = do putStrLn $ bookName ++ " " ++ show (read c :: Int) ++ "\n"
                                              putStrLn t
                                              putStrLn ""
 
-chapPar :: [(Chapter, [Translation])] -> [(Chapter, String)]
-chapPar cts = map (\(c, ts) -> (c, intercalate " " ts)) cts
+chapPar :: [(Chapter, [(Verse, Translation)])] -> [(Chapter, String)]
+chapPar cts = map (\(c, vts) -> (c, intercalate " " $ map (\(v, t) -> v ++ " " ++ t) vts)) cts
+
+htmlOf :: [(Chapter, [(Verse, [Translation])])] -> String
+htmlOf cvts = concat $ map htmlCvt cvts
+       where htmlCvt (c, vts) = "<span class=\"chapter\"><span class=\"ch-num\">" 
+                                      ++ show (read c :: Int) ++ "</span>"
+                                      ++ concat (map htmlVt vts)
+                                      ++ "</span><br></br>\n\n"
+             htmlVt (v, ts) = "<span class=\"verse\"><span class=\"verse-num\"><sup>" ++ v ++ "</sup></span> " 
+                              ++ concat (map htmlTrans ts) ++ "</span><br>\n"
+             htmlTrans t = "<span class=\"translation\">" ++ t ++ " </span>"
            
-groupByChapter :: [(Verse, Translation)] -> [(Chapter, [Translation])]
+groupByChapter :: [(Verse, [Translation])] -> [(Chapter, [(Verse, [Translation])])]
 groupByChapter verses = let verseChapter = map parseVerse verses in
                         let chapterGroups = groupBy sameChapter verseChapter in
                         map combineTranslations chapterGroups
-                        where parseVerse (v, t) = (parseName v, t) -- "Ecclesiastes 13:19" -> (13, 19)
+                        where parseVerse (nm, ts) = let (c, v) = parseName nm in
+                                                  (c, (v, ts)) -- "Ecclesiastes 13:19" -> (13, 19)
                               parseName n = let str = splitOn ":" $ drop nameLen n in
                                             (str !! 0, str !! 1)
-                              sameChapter ((c1, v1), t1) ((c2, v2), t2) = (c1 == c2)
-                              combineTranslations xs = (read (fst $ fst $ head xs) :: Int, map snd xs)
+                              sameChapter (c1, (v1, ts1)) (c2, (v2, ts2)) = (c1 == c2)
+                              combineTranslations xs = (fst $ head xs, map snd xs)
 
 main :: IO ()
 main = do putStrLn "Parsing"
@@ -80,13 +91,17 @@ main = do putStrLn "Parsing"
           -- The first word of each translation-group is the verse name
           -- TODO remove ending \r chars?
           let lns = verseAndTranslations contents
-          let (header, bibLns) = (head lns, tail lns)
-          let (g', lns_rnd) = mapWithGen gen pickTrans bibLns
-          let chapterAndVerse = groupByChapter lns_rnd
-          let chapterAndPar = chapPar chapterAndVerse 
+          -- let (g', lns_rnd) = mapWithGen gen pickTrans bibLns
+          let chapterAndVerse = groupByChapter lns
+          -- let chapterAndPar = chapPar chapterAndVerse 
           -- putStrLn "original"
           -- prettyPrint lns
           -- putStrLn "new"
           -- prettyPrint2 lns_rnd
-          putStrLn "chapters"
-          prettyPrintChapter chapterAndPar
+          -- putStrLn "chapters"
+          -- prettyPrintChapter chapterAndPar
+          putStrLn "HTML result"
+          let preamble = "<script src=\"http://code.jquery.com/jquery.min.js\"></script>\n\n<head> <link rel=\"stylesheet\" href=\"style-bible.css\"> </head>"
+          let htmlRes = preamble ++ "<body><h1>Ecclesiastes</h1><br>" ++ htmlOf chapterAndVerse ++ "</body>"
+          -- putStrLn htmlRes
+          writeFile "bible.html" htmlRes
